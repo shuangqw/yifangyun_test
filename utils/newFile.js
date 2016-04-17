6d49bc29e6485dca353328f6d25ca31e8165c275
shuang <qi.weishuang9@gmail.com>
2016-04-17 08:52:27 +0800

    newfolder.js

---
+++ utils/newFile.js
@@ -0,0 +1,18 @@
+module.exports = function newfile(client,fileName,done) {   //新建文件夹，client对象，文件夹名称，done
+        client
+        .pause(5000)
+        .waitForExist('#fileList',5000)
+        .click('#create_new')
+        .waitForExist('[data-type="1"]', 5000)
+        .click('[data-type="1"]')
+        .setValue('#new_item_name', fileName)
+        .click('.submit', function(){
+            client
+            .pause(2000)
+            .getText('.file-name', function(err,text){
+                text[0].should.equal(fileName.toString()+'.doc');
+
+                done();
+            });
+        });
+};
--- utils/newfolder.js
+++ utils/newfolder.js
@@ -5,7 +5,13 @@ module.exports = function newfolder(client,folderName,done) {   //新建文件�
         .waitForExist('[data-type="0"]',8000)
         .click('[data-type="0"]')
         .setValue('#new_item_name', folderName)
-        .click('.submit',function(){
-          done();
+        .click('.submit', function(){
+            client
+            .pause(1000)
+            .getText('.file-name', function(err,text){
+                text[0].should.equal(folderName.toString());
+
+                done();
+            });
         });
 };
